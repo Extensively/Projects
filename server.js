899338f1288f9d7d075d7ebf4632a8a3efda657a
Extensively <a.smith@bcc.vic.edu.au>
2025-09-17 00:27:15 +0000

    modified:   package.json 	modified:   server.js

--- server.js
+++ server.js
@@ -6,23 +6,20 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
-let users = {};          // { socketId: username }
-let chatHistory = [];    // all messages in order
+let users = {};
+let chatHistory = [];
 
 io.on("connection", (socket) => {
-
   socket.on("set_username", (username) => {
     users[socket.id] = username;
 
-    // Notify all clients about the join
+    // join message
     const joinMsg = { username: "System", msg: `${username} joined`, type: "system" };
     chatHistory.push(joinMsg);
     io.emit("chat_message", joinMsg);
 
-    // Send existing chat history to the new user
+    // send chat history to new user
     socket.emit("chat_history", chatHistory);
-
-    // Update online users for everyone
     io.emit("update_users", Object.values(users));
   });
 
@@ -35,15 +32,13 @@ io.on("connection", (socket) => {
     const username = users[socket.id];
     if (username) {
       delete users[socket.id];
-
       const leaveMsg = { username: "System", msg: `${username} left`, type: "system" };
       chatHistory.push(leaveMsg);
       io.emit("chat_message", leaveMsg);
-
       io.emit("update_users", Object.values(users));
     }
   });
+});
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-});
